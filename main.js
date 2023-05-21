5d83135024d0ce211bd3937a2467b88c2f736d97
nacho83-create <116385512+nacho83-create@users.noreply.github.com>
2023-05-21 15:46:02 -0300

    definitivo de index y discografia
    se hace responsivo ambas paginas , con formulario validado, y api consumida en index con lista de ganadores! falta definir paleta de colores

--- main.js
+++ main.js
@@ -26,4 +26,53 @@ function tabla(datos){
         `    
     }
 } 
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const telefono = document.getElementById("telefono")
+const email = document.getElementById("email")
+const direccion = document.getElementById("direccion")
+const lugar = document.getElementById("lugar")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings=""
+    let entrar= false
+    let regexName=/^[A-Za-z]+$/
+    let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    let regexPhone=/^\(?([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{6})$/
+
+    if (!regexName.test(nombre.value)){
+        warnings+=`Inavlid Name <br>`
+        entrar=true
+    }
+    if (!regexName.test(apellido.value)){
+        warnings+=`Invalid UserName <br>`
+        entrar=true
+    }
+    if (!regexPhone.test(telefono.value)){
+        warnings+=`Inavlid Phone <br>`
+        entrar=true
+    }
+    if (!regexEmail.test(email.value)){
+        warnings+=`Inavlid Email <br>`
+        entrar=true
+    }
+    if (direccion.value.length<1){
+        warnings+=`Invalid Address <br>`
+        entrar=true
+    }
+    if (!regexName.test(lugar.value)){
+        warnings+=`Invalid Province/Country  <br>`
+        entrar=true
+    }
+    if (entrar){
+        parrafo.innerHTML= warnings
+    }else{
+        parrafo.innerHTML= "Sent Successfully"
+    }
+    
+
+})
+
 
